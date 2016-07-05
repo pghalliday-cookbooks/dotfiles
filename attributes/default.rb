24ae01ffa77df1fc44293b2638c869e328a77bcc
Peter Halliday <pghalliday@gmail.com>
2016-07-05 22:18:29 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['avr_development']['user'] = nil
-default['avr_development']['group'] = nil
-default['avr_development']['home'] = nil
-default['avr_development']['ssh']['id_rsa'] = '/root/.ssh/id_rsa'
-default['avr_development']['ssh']['authorized_keys'] = '/root/.ssh/authorized_keys'
+default['dotfiles']['user'] = nil
+default['dotfiles']['group'] = nil
+default['dotfiles']['home'] = nil
+default['dotfiles']['ssh']['id_rsa'] = '/root/.ssh/id_rsa'
+default['dotfiles']['ssh']['authorized_keys'] = '/root/.ssh/authorized_keys'
